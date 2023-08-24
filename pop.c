a93d24ae74614e0c29c2511771c495afae1c7416
WanguiK <wanguikings@gmail.com>
2023-08-24 00:00:20 -0700

    updates

--- pop.c
+++ pop.c
@@ -25,3 +25,19 @@ void _pop(stack_t **stack, unsigned int line_number)
 	}
 	free(top);
 }
+/**
+ * free_dlistint - free a list
+ * @head: pointer to first node
+ *
+ */
+void free_dlistint(stack_t *head)
+{
+	stack_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
--- push.c
+++ push.c
@@ -28,19 +28,3 @@ void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 	}
 	*stack = top;
 }
-/**
- * free_dlistint - free a list
- * @head: pointer to first node
- *
- */
-void free_dlistint(stack_t *head)
-{
-	stack_t *tmp;
-
-	while (head != NULL)
-	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
-	}
-}
